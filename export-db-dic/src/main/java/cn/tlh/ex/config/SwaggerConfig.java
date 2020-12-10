6e0589eb845af76fb26cda356b918292454838d3
tanglinghan <im.linghan@qq.com>
2020-12-10 14:16:17 +0800

    swagger配置

---
+++ export-db-dic/src/main/java/cn/tlh/ex/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package cn.tlh.ex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * http://localhost:8079/doc.html
+ *
+ * @author TANG
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket customDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.tlh.ex.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * name:开发者姓名
+     * url:开发者网址
+     * email:开发者邮箱
+     *
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //标题
+                .title("export-db-dic")
+                //文档接口的描述
+                .description("导出数据库词典(MySQL)")
+                .contact(new Contact("蛮吉", "", " "))
+                //版本号
+                .version("1.0.0")
+                .build();
+    }
+
+}
